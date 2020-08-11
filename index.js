7074a60757a52b7cf8fbe87b0997d98855eb709c
Otávio <otavio.stein@gmail.com>
2020-08-11 15:20:59 -0300

    webscrapping

---
+++ index.js
@@ -0,0 +1,36 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+(async () => {
+  	const browser = await puppeteer.launch();
+  	const page = await browser.newPage();
+	await page.goto('https://instagram.com/rocketseat_oficial');
+	  
+
+  	const imgList = await page.evaluate(()=>{
+		//toda essa função será executada no browser
+
+		//pegando todas as imagens que estão na parte de posts
+		const NodeList = document.querySelectorAll('article img');
+		//transformar o NodeList em array
+		const imgArray = [...NodeList];
+		//transformar os nodes(elementos html) em objetos javascript
+		const imgList = imgArray.map( img => ({
+			src: img.src
+		}));
+
+		console.log(imgList);
+		//colocar para fora da função
+		return imgList;
+	});
+
+	//escrever os dados em um arquivo local (json)
+	fs.writeFile('instagram.json', JSON.stringify(imgList, null, 2), err =>{
+		if(err) throw new Error('something went wrong');
+
+		console.log('imagens baixadas');
+	});
+
+
+  	await browser.close();
+})();
